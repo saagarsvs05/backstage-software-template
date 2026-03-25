cc748a6e81c315dd16fa80ebcec16bb0eb5b2556
saagarsvs05 <saagar.svs05@gmail.com>
2026-03-25 09:26:17 +0000

    added IAC

---
+++ IAC/template/terraform/main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "${var.app_name}-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "${var.app_name}-app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {}
+}
---
+++ IAC/template/terraform/variable.tf
@@ -0,0 +1,3 @@
+variable "resource_group_name" {}
+variable "location" { default = "Central India" }
+variable "app_name" {}
